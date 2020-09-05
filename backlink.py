73b4107af56c1a16135e02ff3bef3d6125edca13
lawiedc <github-dcl@hoopoes.com>
2020-09-05 08:27:47 +0100

    Add more sample code

--- backlink.py
+++ backlink.py
@@ -6,8 +6,8 @@
 # - check if in wayback machine ( https://archive.org/help/wayback_api.php )
 # - if yes, update url
 # - if no, make a note that this needs to be regenerated on hoopoes.com
-import sys
-print(sys.path)
+
+# Sample code from https://pynative.com/python-mysql-database-connection/
 
 import mysql.connector
 from mysql.connector import Error
@@ -24,6 +24,28 @@ core_query = """SELECT
     and link not like '%web.archive.org%'
     """
 
+try:
+    connection = mysql.connector.connect(host='localhost',
+                                         database='Electronics',
+                                         user='pynative',
+                                         password='pynative@#29')
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+        cursor = connection.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("You're connected to database: ", record)
+
+except Error as e:
+    print("Error while connecting to MySQL", e)
+finally:
+    if (connection.is_connected()):
+        cursor.close()
+        connection.close()
+        print("MySQL connection is closed"
+
+)
 
 # Open database
 # Set up query
